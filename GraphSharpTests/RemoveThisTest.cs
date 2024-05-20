5333f4b98825a4a9b53a5c9e7e87e802e3331e88
Nate Adams <psiberknetic@gmail.com>
2024-05-20 11:35:59 -0500

    Initial Project setup

---
+++ GraphSharpTests/RemoveThisTest.cs
@@ -0,0 +1,14 @@
+using Shouldly;
+
+namespace GraphSharpTests
+{
+    [TestClass]
+    public class RemoveThisTest
+    {
+        [TestMethod]
+        public void OnePassingTestToSetUpActions()
+        {
+            true.ShouldBeTrue();
+        }
+    }
+}
