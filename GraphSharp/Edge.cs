fa9f6adcfa1fb16cfb944f5dd64820ec02bc7825
Nate Adams <psiberknetic@gmail.com>
2024-05-23 08:15:43 -0500

    First version of key classes

---
+++ GraphSharp/Edge.cs
@@ -0,0 +1,12 @@
+﻿namespace GraphSharp
+{
+    public class Edge
+    {
+        internal Edge() { }
+        private readonly Guid _id;
+        public Guid Id => _id;
+        public string? Type { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
---
+++ GraphSharp/Graph.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphSharp
+{
+    public class Graph
+    {
+        public IEnumerable<Node> Nodes { get; }
+        public IEnumerable<Edge> Edges { get; }
+        public Graph() { }
+    }
+}
